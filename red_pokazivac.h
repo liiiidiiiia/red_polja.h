8c53e0f5f8d81e676217bd7ec1823116d9fd7075
marinicblaza <blaza15@hotmail.com>
2013-12-15 23:06:24 +0100

    Create red_pokazivac.h

---
+++ red_pokazivac.h
@@ -0,0 +1,45 @@
+struct osoba {
+    int ai,bi,ci,di,ei;
+    int datum;
+    char ime[25],prezime[30],spol[1];
+    osoba*sljedeci;
+};
+
+struct tred {
+    osoba* front, *rear;
+};
+
+int AddOne(int n) {
+    return((n+1)%10000);
+}
+
+int InitQ(tred*red) {
+    osoba*glava=new osoba;
+    glava->sljedeci=NULL;
+    red->front=glava;
+    red->rear=glava;
+}
+
+bool IsEmptyQ(tred*red) {
+    if(red->rear==red->front)return 1;
+    return 0;
+}
+
+void EnQueueQ(osoba*pacijent, tred*red) {
+    red->rear->sljedeci=pacijent;
+    pacijent->sljedeci=NULL;
+    red->rear=pacijent;
+}
+
+osoba*FrontQ(tred*red) {
+    osoba*pacijent;
+    pacijent=red->front->sljedeci;
+    return pacijent;
+}
+
+void DeQueueQ(tred*red) {
+    osoba*glava=new osoba;
+    glava=red->front;
+    red->front=glava->sljedeci;
+    delete glava;
+}
